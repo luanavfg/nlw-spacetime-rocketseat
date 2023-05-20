12bacbda1ba0f4f3171572f19ac76d680ccf136a
Luana <luanavefago1@gmail.com>
2023-05-20 18:20:32 -0300

    feat: classe 03 - split home page in multiple components

---
+++ aulas/web/src/components/EmptyMemories.tsx
@@ -0,0 +1,12 @@
+export function EmptyMemories() {
+  return (
+    <div className="flex h-full items-center justify-center">
+      <p className="loading-relaxed w-[360px] text-center">
+        Você ainda não registrou nenhuma lembrança, comece a{' '}
+        <a href="" className="underline hover:text-gray-50">
+          criar agora!
+        </a>
+      </p>
+    </div>
+  )
+}
